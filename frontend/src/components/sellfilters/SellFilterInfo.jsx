4b5a5cf5c25c462d95726956d12c0c45d0a45ee3
PamelaFloresMedrano <plflores2@espe.edu.ec>
2023-01-11 16:22:19 -0500

    design vendors 2.0

--- frontend/src/components/sellfilters/SellFilterInfo.jsx
+++ frontend/src/components/sellfilters/SellFilterInfo.jsx
@@ -3,9 +3,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { Store } from '../../Store'
-import Card from '@mui/material/Card';
-import { Button, CardActionArea} from '@mui/material';
-import { Swiper } from 'swiper/react';
 import "./Cards.css";
 
 const SellFilterInfo = ({user}) => {
@@ -55,29 +52,19 @@ const SellFilterInfo = ({user}) => {
     }
 
   return (
-    <Swiper>
-    <Card sx={{ maxWidth: 345 }}>
-    <CardActionArea>
-    <div className="pastel-frame">
+        
     <div className="sell-fill-group" key={user._id}>
         <div className="sell-fill-header">
             <img src={user.image} alt={user.name} />
         </div>
         <div className="sell-fill-body">
             
-            <Link to={`../seller/${user._id}`}>{user.name} <FontAwesomeIcon icon={faEye} /></Link>
-           
-            {existUser && sellerExists ? ( <Button variant="contained" color="error" size="medium"><span onClick={() => handlerUnfollow(user)}>Dejar de seguir</span></Button>) : (<Button variant="contained" size="medium" color="success"><span key={user._id} onClick={handlerFollow}>Seguir</span></Button>)}
-           
-
+        <Link to={`../seller/${user._id}`}>{user.name} <FontAwesomeIcon icon={faEye} /></Link>
+            {existUser && sellerExists ? (<span onClick={() => handlerUnfollow(user)} className='unfollow'>Dejar de seguir</span>) : (<span key={user._id} onClick={handlerFollow} className='follow'>Seguir</span>)}
             <span className="date">Miembro Desde: {(user.createdAt).slice(0, 10)}</span>
         </div>
     </div>
-    </div>
-    </CardActionArea>
-    </Card>
-   
-    </Swiper>
+    
   )
 }
 
