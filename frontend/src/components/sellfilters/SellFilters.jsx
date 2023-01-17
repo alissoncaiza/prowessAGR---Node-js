5c4f0de819d31aa89ee8d9c7bbc220ca18b5c72f
PamelaFloresMedrano <plflores2@espe.edu.ec>
2023-01-17 18:02:18 -0500

    Card.css to sellers.css

--- frontend/src/components/sellfilters/SellFilters.jsx
+++ frontend/src/components/sellfilters/SellFilters.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import SellFilter from "./SellFilter";
-import "./Cards.css";
+
 
 const SellFilters = () => {
   const [seller, setSeller] = useState([]);
