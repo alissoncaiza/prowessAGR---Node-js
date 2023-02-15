64d86157052d5659d17f4c67c38a9dd736c036db
Faby-bg <diana.barrera1402@gmail.com>
2023-02-15 16:59:54 -0500

    BackEnd

--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -3,7 +3,7 @@ import * as categoryController from "../controllers/categoryController.js";
 
 const router = Router();
 
-//for fetch or get category from db --- PULL CATEGORY
+// Para buscar u obtener categoría de la base de datos
 router.get("/", categoryController.getCategories);
 router.get("/:id", categoryController.getCategoryById);
 
