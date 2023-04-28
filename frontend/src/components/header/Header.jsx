a05ba93c14b3228fb92e0737adf958f57f212c8e
estevennac <estevennac@gmail.com>
2023-04-27 21:48:23 -0500

    navbar

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,14 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
   faLock,
   faShoppingBag,
   faUser,
+  faBars,
 } from "@fortawesome/free-solid-svg-icons";
 import "../../pages/home/home.css";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { Store } from "../../Store";
+import {useRef} from "react";
 import "./header.css"
 const Header = () => {
   const navigate = useNavigate();
@@ -25,75 +27,123 @@ const Header = () => {
     navigate("/login");
   };
 
+  const navRef = useRef();
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
-    <div className="header-row " >
-      <div className="header-menu">
-        <div className="header-logo">
+  <>
+    <nav className="navbar">
+      <div className="nav-container">
+        <div className="nav-logo">
           <Link to="/">
             <img
               className="header-logo-image"
               src="./assets/images/others/prowess-logo1.png"
-              //src="./assets/images/others/prowess-logo2.png"
-
               alt=""
             />
           </Link>
         </div>
-        <div className="header-nav" >
-          <NavLink to="/" activeclassname="active">
-            Inicio
-          </NavLink>
-          <NavLink to="/shop" activeclassname="active">
-            Tienda
-          </NavLink>
-          <NavLink to="/sellers" activeclassname="active">
-            Vendedores
-          </NavLink>
-        </div>
-        <div className="header-action">
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Inicio
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/shop"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Tienda
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/sellers"
+              activeClassName="active"
+              className="nav-links"
+              onClick={handleClick}
+            >
+              Vendedores
+            </NavLink>
+          </li>
+        </ul>
+          
+        <ul className={click ? "nav-account active" : "nav-account"}>
           {userInfo && (
             <>
-              <Link to="/">
-                <FontAwesomeIcon icon={faHome} />
-              </Link>
-              <Link to="/account">
-                <FontAwesomeIcon icon={faUser} />
-                {/* Numeración de seguidos sobre ícono de usuario
-                  wish.wishItems.length ? (
-                  <span className="header-cart-badge">
-                    {wish.wishItems.length}
-                  </span>
-                ) : (
-                  <span className="header-cart-badge">0</span>
-                )*/}
-              </Link>
-              <Link to="/cart">
-                <FontAwesomeIcon icon={faShoppingBag} />
-                {cart.cartItems.length ? (
-                  <span className="header-cart-badge">
-                    {cart.cartItems.length}
-                  </span>
-                ) : (
-                  <span className="header-cart-badge">0</span>
-                )}
-              </Link>
+              <div className="header-action">
+                <Link to="/" >
+                  <FontAwesomeIcon icon={faHome} />
+                </Link>
+              </div>
+               
+              <div className="header-action">
+                <Link to="/account" >
+                  <FontAwesomeIcon icon={faUser} />               
+                </Link>
+              </div>
+
+              <div className="header-action">
+                <Link to="/cart" >
+                  <FontAwesomeIcon icon={faShoppingBag} />
+                  {cart.cartItems.length ? (
+                    <span className="header-cart-badge">
+                      {cart.cartItems.length}
+                    </span>
+                  ) : (
+                    <span className="header-cart-badge">0</span>
+                  )}
+                </Link> 
+              </div>
             </>
-          )} 
+          )}
 
           {userInfo ? (
-            <span className="logout" onClick={signouthandler}>
-              <FontAwesomeIcon icon={faLock} />
-              <span className="login">Salir</span>
-            </span>
+           <div className="header-action">
+              <span className="logout" onClick={signouthandler} >
+                <FontAwesomeIcon icon={faLock} /> 
+              <span className="salir">Salir</span>
+             </span>
+            </div>   
           ) : (
-            <Link to="/login">
-              <FontAwesomeIcon icon={faLock} />
-              <span className="login">Iniciar sesión</span>
-            </Link>
+          <NavLink
+            exact
+            to="/login"
+            activeClassName="active"
+            className="nav-links"
+            onClick={handleClick}
+          >
+            Inicio Sesión
+          </NavLink>
           )}
+          
+        </ul>
+          
+        <div className="nav-icon" onClick={handleClick}>
+        <FontAwesomeIcon icon={faBars} />
         </div>
       </div>
-    </div>
+    </nav>
+  </>
+
   );
 };
 
