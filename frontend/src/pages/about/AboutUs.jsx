4a5595f17547f5977facf503612867908990e85b
RafaelOrtiz8995 <raortiz4@espe.edu.ec>
2023-05-18 17:11:55 -0500

    BotonesFormReg

--- frontend/src/pages/about/AboutUs.jsx
+++ frontend/src/pages/about/AboutUs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+
 import "./About.css";
 const AboutUs = () => {
   return (
